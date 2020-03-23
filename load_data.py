9f86837e96590c67f7d8d7481122ffbf0727c4b4
Xiao Li <xsli@arwen.Berkeley.EDU>
2020-03-23 15:28:13 -0700

    merging county and hospital level data

--- load_data.py
+++ load_data.py
@@ -10,6 +10,7 @@ from functions import load_respiratory_disease_data
 from functions import load_tobacco_use_data
 from os.path import join as oj
 from sklearn.model_selection import train_test_split
+#from load_data import load_county_level
 
 outcome_cases = '#Cases_3/22/2020'
 outcome_deaths = '#Deaths_3/22/2020'
@@ -43,6 +44,28 @@ def load_county_level(ahrf_data = 'data/hrsa/data_AHRF_2018-2019/processed/df_re
     df['#FTEHospitalTotal2017'] = df['#FTETotalHospitalPersonnelShortTermGeneralHospitals2017'] + df['#FTETotalHospitalPersonnelSTNon-Gen+LongTermHosps2017']
 
     return df
+
+
+def merge_hospital_and_county_data(hospital_info_keys, county_info_keys):
+    
+    hospitals = pd.read_csv("data/hospital_level_info/02_hospital_info.csv")
+    county_to_fips = dict(zip(zip(hospitals['COUNTY'], hospitals['STATE']), hospitals['COUNTYFIPS']))
+    hospital_level = pd.read_csv("data/hospital_level_info/03_hospital_level_info_merged.csv")
+    def map_county_to_fips(name, st):
+        if type(name) is str:
+            #name = re.sub('[^a-zA-Z]+', '', county_name).lower()
+            if (name, st) in county_to_fips and county_to_fips[(name, st)] != "NOT AVAILABLE":
+                return int(county_to_fips[(name, st)])
+        return np.nan
+    hospital_level['countyFIPS'] = hospital_level.apply(lambda x: map_county_to_fips(x['County Name_y'], x['State_x']), axis=1).astype('float')
+    county_level = load_county_level()
+    if county_info_keys != 'all':
+        county_level = county_level[county_info_keys]
+    if hospital_info_keys != 'all':
+        hospital_level = hospital_level[hospital_info_keys]
+    hospital_county_merged = hospital_level.merge(county_level, how='left', on='countyFIPS')
+    return hospital_county_merged
+
 
 
 def split_data(df):
