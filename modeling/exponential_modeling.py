35ba916408e9998eb451c5aa1650042d3933d32e
naltieri <nl.altieri@gmail.com>
2020-03-26 21:10:09 -0700

    shared model

--- modeling/exponential_modeling.py
+++ modeling/exponential_modeling.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import seaborn as sns
-from viz import viz
+# from viz import viz
 from bokeh.plotting import figure, show, output_notebook, output_file, save
 from functions import merge_data
 from sklearn.model_selection import RandomizedSearchCV
@@ -77,16 +77,86 @@ def estimate_deaths(df, method="exponential", target_day=np.array([1])):
         predicted_death_rate = np.array(estimate_death_rate(df, method="constant")['predicted_death_rate'])
         predicted_deaths = predicted_cases * predicted_death_rate
         
+
+        
     df[f'predicted_deaths_{method}'] = predicted_deaths
         
     return df
         
-def create_leave_one_day_out_valid(df):
-    
+def create_leave_one_day_out_valid(df):    
     df2 = copy.deepcopy(df)
     days = len(df['deaths'].values[0])
     for i in range(len(df)):
         df2['deaths'].values[i] = df2['deaths'].values[i][0:(days-1)]
         df2['cases'].values[i] = df2['cases'].values[i][0:(days-1)]
     return df2
+
+def create_shared_simple_dataset(train_df):
+    """
+    Create a very simple dataset for creating a shared Poisson GLM across counties:
+    Input: train_df: A df with county level deaths
+    Output:
+    X_train: a list of lists of the form [np.log(previous_days_death_count+1),1]
+    y_train: a list of with elements - current_days_death_count
+    """
+    X_train = []
+    y_train = []
+    county_deaths = list(train_df['deaths'])
+    for i in range(len(county_deaths)):       
+        deaths = county_deaths[i]
+        for j in range(len(deaths)):
+            # Only add a point if total death are greater than 3. (3 chosen abritrarily)
+            if deaths[j] > 3: 
+                X_train.append([np.log(deaths[j-1]+1),1])
+                y_train.append(deaths[j])
+    return X_train, y_train
+
+def _fit_shared_exponential(X_train,y_train):
+    """
+    Input: 
+    X_train: A list of lists, where each sublist is [log(prev_days_death+1),1], where the 1 is for the bias term for the model
+    y_train: A list of deaths for current day under the model
+    Output: 
+    Trains a poisson GLM with parameters shared across all counties.
+    """
+
+    model = sm.GLM(y_train, X_train,
+               family=sm.families.Poisson())
+    model = model.fit()
+    return model 
+
+def fit_and_predict_shared_exponential(train_df,test_df):
+    """
+    fits a poisson glm to all counties in train_df and makes prediction for the most recent day for test_df
+    Input:
+    train_df, test_df: dataframes with county level deaths:
+    Output:
+    predicted_deaths: a list containing predictions for the current death count for current day of test_df
+    """
+    X_train, y_train = create_shared_simple_dataset(train_df)
+    model = _fit_shared_exponential(X_train,y_train)
+    predicted_deaths = get_shared_death_predictions_for_current_day(test_df,model)
+    return predicted_deaths
+
+
+
+def get_shared_death_predictions_for_current_day(test_df,model):
+    """Predicts the death total for the most recent day in test_df
+    Input:
+    test_df: dataframes with county level deaths:
+    Output:
+    predicted_deaths: a list containing predictions for the current death count for current day of test_df
+
+    """
+
+    county_deaths = list(test_df['deaths'])
+    predicted_deaths = []
+    for deaths in county_deaths:
+        predicted_deaths.append(model.predict([[np.log(deaths[-2]+1),1]])[0])
+    return predicted_deaths 
+
+
+
+
+
 
