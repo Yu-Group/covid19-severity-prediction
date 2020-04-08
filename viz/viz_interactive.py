8272ef4b654269c4d4618402648efd342eefe961
Chandan Singh <chandan_singh@berkeley.edu>
2020-04-08 16:10:56 -0700

    add lat/lon to hosp sheet

--- viz/viz_interactive.py
+++ viz/viz_interactive.py
@@ -627,7 +627,7 @@ def viz_index_animated(d, NUM_DAYS_LIST, out_name="results/hospital_index_animat
     dd['Severity Index'] = flat_list([d[f"Severity Index {i}-day"].values for i in NUM_DAYS_LIST])
     today = datetime.today().strftime("%B %d")
     new_key = f'Total Deaths at hospital by {today}'
-    dd = dd.rename(columns={'Total Deaths Hospital': new_key})dd
+    dd = dd.rename(columns={'Total Deaths Hospital': new_key})
     fig = px.scatter(dd, x=new_key, 
                  y='Predicted new deaths at hospital', 
                  animation_frame="Days in the future", 
