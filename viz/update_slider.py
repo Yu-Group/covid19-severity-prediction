28bfba4ebf15e78cae86c973b35e7af9f4d6d5d6
James Duncan <jpduncan@berkeley.edu>
2020-04-07 17:37:15 -0700

    Slider viz: use county center of population for lat/lon

--- viz/update_slider.py
+++ viz/update_slider.py
@@ -8,16 +8,28 @@ from modeling import fit_and_predict
 from fit_and_predict import add_preds
 from viz_interactive import plot_counties_slider
 import load_data
+import numpy as np
 import pandas as pd
 
 data_dir = sys.argv[1]
 lat_lon_file = sys.argv[2]
 
-# usage: python update_slider.py data_dir county_lat_long.csv
+# usage: python update_slider.py path/to/data county_pop_centers.csv
 
 if __name__ == "__main__":
+    # load in county data
     df = load_data.load_county_level()
-    county_lat_lon = pd.read_csv(data_dir + '/' + lat_lon_file)
+    # add lat and lon to the dataframe
+    county_lat_lon = pd.read_csv(
+        data_dir + '/' + lat_lon_file,
+        dtype={'STATEFP': str, 'COUNTYFP': str}
+    )
+    county_lat_lon['fips'] = (county_lat_lon['STATEFP'] + county_lat_lon['COUNTYFP']).astype(np.int64)
+    # add predictions
     df = add_preds(df, NUM_DAYS_LIST=[1, 2, 3], cached_dir=data_dir)
-    df = df.join(county_lat_lon.set_index('fips'), on='countyFIPS', how='left')
+    # join lat / lon to df
+    df = df.join(county_lat_lon.set_index('fips'), on='countyFIPS', how='left').rename(
+        columns={'LATITUDE' : 'lat', 'LONGITUDE' : 'lon'}
+    )
+    # create plot
     plot_counties_slider(df)
