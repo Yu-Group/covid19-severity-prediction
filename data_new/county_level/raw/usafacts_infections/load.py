5f7935c978f99b62115132dba559805a2c3dee25
Ubuntu <ubuntu@ip-172-31-29-26.us-west-2.compute.internal>
2020-04-08 21:49:07 +0000

    add usafacts_infections

--- data_new/county_level/raw/usafacts_infections/load.py
+++ data_new/county_level/raw/usafacts_infections/load.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python3
+
 import pandas as pd
 import numpy as np
 from os.path import join as oj
+
 
 def load_usafacts_infections(data_dir = "./"):
     ''' Load in USA Facts daily COVID-19 cases and deaths data
@@ -18,14 +21,17 @@ def load_usafacts_infections(data_dir = "./"):
     usafacts_data_cases = oj(data_dir, 'confirmed_cases.csv')
     usafacts_data_deaths = oj(data_dir, 'deaths.csv')
   
-    cases = pd.read_csv(usafacts_data_cases, encoding="iso-8859-1", index_col=0).T
-    deaths = pd.read_csv(usafacts_data_deaths, encoding="iso-8859-1", index_col=0).T
-    if not 'countyFIPS' in deaths.keys():
-        deaths = pd.read_csv(usafacts_data_deaths, encoding="utf-8", index_col=0).T
+    cases = pd.read_csv(usafacts_data_cases, encoding="utf-8", index_col=0, dtype=str).T
+    deaths = pd.read_csv(usafacts_data_deaths, encoding="utf-8", index_col=0, dtype=str).T
+    cases.countyFIPS = cases.countyFIPS.str.zfill(5)
+
+    deaths.countyFIPS = deaths.countyFIPS.str.zfill(5)
     # change to int type
     for col in cases.columns:
         if not 'county' in col.lower() and not 'state' in col.lower():
             cases[col] = cases[col].astype(float).astype(int)
+    for col in deaths.columns:
+        if not 'county' in col.lower() and not 'state' in col.lower():
             deaths[col] = deaths[col].astype(float).astype(int)
     # rename column names
     cases = cases.rename(columns={k: '#Cases_' + k for k in cases.keys()
@@ -36,26 +42,17 @@ def load_usafacts_infections(data_dir = "./"):
                                     if not 'county' in k.lower()
                                     and not 'state' in k.lower()})
 
-    deaths.countyFIPS = deaths.countyFIPS.astype(int)
-    cases.countyFIPS = cases.countyFIPS.astype(int)
-    cases = cases[cases.countyFIPS != 0]  # ignore cases where county is unknown
+    cases = cases[cases.countyFIPS != '00000']  # ignore cases where county is unknown
     cases = cases.groupby(['countyFIPS']).sum().reset_index()  # sum over duplicate counties
-    deaths = deaths[deaths.countyFIPS != 0]
+    deaths = deaths[deaths.countyFIPS != '00000']
     deaths = deaths.groupby(['countyFIPS']).sum().reset_index()
 
     df = pd.merge(cases, deaths, how='left', on='countyFIPS')
     df = df.fillna(0)
-    
-    # add time-series keys
-    deaths_keys = [k for k in df.keys() if '#Deaths' in k and not 'Unnamed' in k]
-    cases_keys = [k for k in df.keys() if '#Cases' in k and not 'Unnamed' in k]
-    deaths = df[deaths_keys].values
-    cases = df[cases_keys].values
-    df['deaths'] = [deaths[i] for i in range(deaths.shape[0])]
-    df['cases'] = [cases[i] for i in range(cases.shape[0])]
-    df['tot_deaths'] = deaths[:, -1]
-    df['tot_cases'] = cases[:, -1]
-    
-    df['countyFIPS'] = df['countyFIPS'].astype(str).str.zfill(5)
-    
+
+    df.to_csv("usafacts_infections.csv") 
     return df
+
+if __name__ == '__main__':
+    load_usafacts_infections()
+    print("load usafacts infections successfully.")
