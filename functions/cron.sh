6923f928b5d35665996246e49507fcc83da6a88d
Ubuntu <ubuntu@ip-172-31-29-26.us-west-2.compute.internal>
2020-04-17 01:15:40 +0000

    update usafacts and nytimes data

--- functions/cron.sh
+++ functions/cron.sh
@@ -3,15 +3,35 @@
 # want these all to run once a day at 8am: 0 8 * * * before each command
 # ex: 0 8 * * * /path/to/cron.sh
 
-REPO_DIR=/home/ubuntu/uploader
+REPO_DIR=/home/ubuntu/new_uploader
 LOG_FILE=$REPO_DIR/functions/update_test.log
 PATH=/home/ubuntu/anaconda3/bin/:$PATH
 export PATH
 rm $LOG_FILE
 
-# update usafacts and nytimes date
-$REPO_DIR/data/nytimes/update_data.sh
-$REPO_DIR/data/usafacts/update_data.sh
+
+cd $REPO_DIR
+git pull origin master
+# update usafacts and nytimes data
+cd $REPO_DIR/data/raw/nytimes_infections
+$(which python3) download.py >> $LOG_FILE
+cd $REPO_DIR/data/processed/nytimes_infections
+$(which python3) clean.py >> $LOG_FILE
+
+cd $REPO_DIR/data/raw/usafacts_infections
+$(which python3) download.py >> $LOG_FILE
+cd $REPO_DIR/data/processed/usafacts_infections
+$(which python3) clean.py >> $LOG_FILE
+
+# push to git
+git add .
+
+# commit to git
+git commit -am "update usafacts and nytimes data"
+
+# push to origin
+git push --quiet https://$GIT_USERNAME:$GIT_PASSWORD@github.com/Yu-Group/covid19-severity-prediction.git 
+
 
 # update severity index gsheet + index viz
 $(which python3) $REPO_DIR/functions/update_severity_index.py >> $LOG_FILE
@@ -29,4 +49,14 @@ $(which python3) $REPO_DIR/functions/update_county_preds.py >> $LOG_FILE
 $(which python3) $REPO_DIR/functions/update_ihme.py >> $LOG_FILE
 
 # after running all scripts need to push to git
-$REPO_DIR/data/push_to_git.sh
+cd $REPO_DIR
+
+# pull from origin
+git pull origin master
+git add .
+
+# commit to git
+git commit -am "daily update"
+
+# push to origin
+git push --quiet https://$GIT_USERNAME:$GIT_PASSWORD@github.com/Yu-Group/covid19-severity-prediction.git 
