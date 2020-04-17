e17f9b9e16c50bbf9e9679516f64ac5bf11f3433
Tiffany Tang <tmtang15@gmail.com>
2020-04-16 21:46:14 -0500

    Update fit_and_predict.py

--- modeling/fit_and_predict.py
+++ modeling/fit_and_predict.py
@@ -136,6 +136,7 @@ def fit_and_predict(df,
     elif method == 'advanced_shared_model':
         if 'neighbor_deaths' not in df.columns:
             neighboring_counties_df = pd.read_csv(oj(parentdir, 'data/county_level/raw/county_ids/county_adjacency2010.csv'))
+            neighboring_counties_df['fipscounty'] = neighboring_counties_df['fipscounty'].astype(str).str.zfill(5)
             county_neighbor_deaths = []
             county_neighbor_cases = []
             county_fips = list(df['countyFIPS'])
