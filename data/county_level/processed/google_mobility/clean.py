b24282ba7162031c121fe11a7b0a32891651b925
Andrew Ilyas <ailyas@mit.edu>
2020-05-15 03:26:51 -0400

    scripts for google mobility

---
+++ data/county_level/processed/google_mobility/clean.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from pathlib import Path
+
+def clean(data_path='../../raw/google_mobility/google.csv',
+            convenience_cols=True):
+    """
+    Clean google mobility data
+
+    Output will have columns:
+    - Country
+    - State/Province
+    - County
+    - Date (YYYY-MM-DD)
+    - Region Type (one of {County, State/Province, Country}, level of granularity)
+    - Sector (one of {parks, retail_and_recreation, transit, workplaces,
+        residential, grocery_and_pharmacy})
+    """
+    # Naming of region granularities
+    rename_dict = {
+        'sub_region_2': 'County',
+        'sub_region_1': 'Region',
+        'country_region': 'Country',
+        'date': 'Date',
+    }
+    sector_dict = {
+        'retail_and_recreation': 'Retail-Recreation',
+        'grocery_and_pharmacy': 'Grocery-Pharmacy',
+        'parks': 'Parks',
+        'transit_stations': 'Transit',
+        'workplaces': 'Workplace',
+        'residential': 'Residential'
+    }
+    sector_dict = {k + '_percent_change_from_baseline': sector_dict[k] for k in sector_dict}
+    
+    # Load raw data
+    goog_df = pd.read_csv(Path(__file__).parent.absolute() / data_path)
+    goog_df = pd.melt(goog_df, id_vars=rename_dict.keys(), value_vars=sector_dict.keys(), 
+                                var_name='Sector', value_name='Percent Change')
+    goog_df = goog_df.rename(columns=rename_dict)
+    goog_df['Sector'] = goog_df['Sector'].apply(lambda x: sector_dict[x])
+
+    for region_type in ['Country', 'Region', 'County']:
+        this_granularity = goog_df[~goog_df[region_type].isna()].index
+        goog_df.loc[this_granularity, 'Region Type'] = region_type
+    goog_df.to_csv('google.csv', index=False)
+
+if __name__ == '__main__':
+    clean()
