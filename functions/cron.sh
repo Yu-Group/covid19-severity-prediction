e3c77f4110da2d5bc318c386e55dd3a3a7756440
= <=>
2020-09-23 04:46:28 +0000

    remove All_counties

--- functions/cron.sh
+++ functions/cron.sh
@@ -4,12 +4,13 @@
 # ex: 0 8 * * * /path/to/cron.sh
 
 REPO_DIR=/home/ubuntu/new_uploader
+DASHBOARD_DIR=/home/ubuntu/covid19-dashboard
 LOG_FILE=$REPO_DIR/functions/update_test.log
 PATH=/home/ubuntu/anaconda3/bin/:$PATH
 export PATH
 rm $LOG_FILE
 
-
+################# update data #####################
 cd $REPO_DIR
 git pull origin master
 # update usafacts and nytimes data
@@ -32,6 +33,12 @@ git commit -am "update usafacts and nytimes data"
 # push to origin
 git push --quiet https://$GIT_USERNAME:$GIT_PASSWORD@github.com/Yu-Group/covid19-severity-prediction.git 
 
+
+
+
+
+
+################# update visualizations #####################
 # update severity index gsheet + index viz
 $(which python3) $REPO_DIR/functions/update_severity_index.py >> $LOG_FILE
 
@@ -43,10 +50,29 @@ $(which python3) $REPO_DIR/functions/update_predictions_plot.py >> $LOG_FILE
 
 # update the search map
 $(which python3) $REPO_DIR/functions/data_engineering.py >> $LOG_FILE
+$(which python3) $REPO_DIR/functions/update_search.py >> $LOG_FILE
 
-
+# move the search map to other repo
+cp $REPO_DIR/results/All_counties/* $DASHBOARD_DIR/results/All_counties
+cp $REPO_DIR/functions/update_search.json $DASHBOARD_DIR/functions
+cp -r $REPO_DIR/results/* $DASHBOARD_DIR/results
+# cp $REPO_DIR/results/search.html $DASHBOARD_DIR/results/
 # cache IHME preds
 # $(which python3) $REPO_DIR/functions/update_ihme.py >> $LOG_FILE
+
+# push the dashboard repo
+cd DASHBOARD_DIR=/home/ubuntu/covid19-dashboard
+# pull from origin
+git pull origin master
+git add .
+
+# commit to git
+git commit -am "daily update"
+
+# push to origin
+git push --quiet https://$GIT_USERNAME:$GIT_PASSWORD@github.com/Yu-Group/covid19-severity-prediction.git 
+
+
 
 # after running all scripts need to push to git
 cd $REPO_DIR
@@ -59,4 +85,4 @@ git add .
 git commit -am "daily update"
 
 # push to origin
-git push --quiet https://$GIT_USERNAME:$GIT_PASSWORD@github.com/Yu-Group/covid19-severity-prediction.git 
+git push --quiet https://$GIT_USERNAME:$GIT_PASSWORD@github.com/Yu-Group/covid19-dashboard.git 
