9120abf1cfd6ebf4ca5c334d427218782c82fc56
= <=>
2020-09-23 05:04:56 +0000

    remove more files

--- results/db-support/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dbHost = 'localhost'; // usually localhost
-$dbUsername = 'root';
-$dbPassword = 'nbuser';
-$dbDatabase = 'locations';
-
-$mysql = new mysqli($dbHost, $dbUsername, $dbPassword, $dbDatabase) or die("Error connecting.<br />File: ".__FILE__);
-?>
