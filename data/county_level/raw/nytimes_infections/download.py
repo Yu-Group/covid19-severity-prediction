1a7ca2afd7df95a5afe8e4b53cd74b5e82c3ee95
Tiffany Tang <tmtang15@gmail.com>
2021-10-12 15:51:06 -0700

    speed up nyt download by only downloading relevant csv

--- data/county_level/raw/nytimes_infections/download.py
+++ data/county_level/raw/nytimes_infections/download.py
@@ -4,10 +4,7 @@ import os
 
 def download_nytimes_infections(data_dir="."):
     os.system("cd {}".format(data_dir))
-    os.system("git clone https://github.com/nytimes/covid-19-data")
-    os.system("cp ./covid-19-data/us-counties.csv .")
-    os.system("rm -rf ./covid-19-data")
-    os.system("mv us-counties.csv nytimes_infections.csv")
+    os.system("wget https://raw.githubusercontent.com/nytimes/covid-19-data/master/live/us-counties.csv -O nytimes_infections.csv")
 
 if __name__ == "__main__":
     download_nytimes_infections()
