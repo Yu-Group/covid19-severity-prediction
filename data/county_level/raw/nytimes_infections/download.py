b2bd991a19ec75f497ff4832624eb60564ee10b0
Tiffany Tang <tmtang15@gmail.com>
2021-10-15 13:08:26 -0700

    Fix NYT download data link

--- data/county_level/raw/nytimes_infections/download.py
+++ data/county_level/raw/nytimes_infections/download.py
@@ -4,7 +4,7 @@ import os
 
 def download_nytimes_infections(data_dir="."):
     os.system("cd {}".format(data_dir))
-    os.system("wget https://raw.githubusercontent.com/nytimes/covid-19-data/master/live/us-counties.csv -O nytimes_infections.csv")
+    os.system("wget https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv -O nytimes_infections.csv")
 
 if __name__ == "__main__":
     download_nytimes_infections()
